709987459ebfbd1448c9c1b91f01747a2dedbaa1
Jakob Sachs <jakobsachs1999@gmail.com>
2022-05-13 23:37:35 +0200

    Done

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 #define LEARNING_RATE 0.01
-#define EPOCHS 1000
+#define EPOCHS 500
 
 double forward_pass(const double *w, const double *x, size_t n)
 {
@@ -19,7 +19,7 @@ double forward_pass(const double *w, const double *x, size_t n)
   return out;
 }
 
-static double step(double x) { return (x >= 0) ? 1 : 0; }
+static double step(double x) { return (x > 0) ? 1 : 0; }
 
 void adjust_weights(double *w, const double *x, double y, double t, size_t n)
 {
@@ -27,21 +27,25 @@ void adjust_weights(double *w, const double *x, double y, double t, size_t n)
 
   for (size_t i = 0; i < n; i++)
   {
-    w[i + 1] += LEARNING_RATE * (t - y) * x[i];
+    w[i + 1] += LEARNING_RATE * (t - y) * x[i]; 
   }
 }
 
 int main(__attribute__((unused)) int argC, __attribute__((unused)) char **argV)
 {
   // learning AND function
-  const double in[4][2] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-  const double t[4] = {0, 0, 0, 1};
+  const double in[8][3] = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
+                           {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
 
-  double w[5] = {0};
+  const double t[8] = {0, 1, 1, 1, 1, 1, 1, 1};
+  int n = 4 - 1;
+  int m = 8;
 
-  for (size_t i = 0; i < 5; i++)
+  double *w = malloc(sizeof(double) * m);
+
+  for (size_t i = 0; i < n; i++)
   {
-    w[i] = (double)rand() / (double)RAND_MAX;
+    w[i] = (double) rand() / (double) RAND_MAX;
   }
 
   for (size_t e = 0; e < EPOCHS; e++)
@@ -51,44 +55,42 @@ int main(__attribute__((unused)) int argC, __attribute__((unused)) char **argV)
       // calculate error
       double error = 0.0;
 
-      for (size_t i = 0; i < 4; i++)
+      for (size_t i = 0; i < m; i++)
       {
-        double out = step(forward_pass(w, in[i], 2));
+        double out = step(forward_pass(w, in[i], n));
         error += pow((out - t[i]), 2);
       }
 
       error = sqrt(error);
 
-      printf("Epoch %zu> Error: %lf\n", e, error);
-
-      printf("Weights: [%.3f, %.3f, %.3f, %.3f, %.3f]\n", w[0], w[1], w[2], w[3], w[4]);
+      printf("Epoch %4ld > Error: %.3lf\n", e, error);
     }
 
-    for (size_t i = 0; i < 4; i++)
+    for (size_t i = 0; i < m; i++)
     {
-      double y = step(forward_pass(w, in[i], 2));
-      adjust_weights(w, in[i], y, t[i], 2);
+      double y = step(forward_pass(w, in[i], n));
+      adjust_weights(w, in[i], y, t[i], n);
     }
   }
 
   printf("\nFinal weights: [");
   srand(time(NULL));
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i < n; i++)
   {
-    w[i] = (double)rand() / (double) RAND_MAX;
-
     printf(" %.3f", w[i]);
   }
   printf("]\n\n");
 
   // do a test prediction
-  for (size_t i = 0; i < 4; i++)
+  for (size_t i = 0; i < m; i++)
   {
-    double out = forward_pass(w, in[i], 2);
-    printf("in:[%d,%d]\tout:%.3f\n", (int) in[i][0], (int) in[i][1], out);
+    double out = step(forward_pass(w, in[i], n));
+    printf("in:[%d,%d,%d]\tout:%d\n", (int) in[i][0], (int) in[i][1],
+           (int) in[i][2], (int) out);
   }
 
   printf("\n\n");
+  free(w);
 
   return 0;
 }
